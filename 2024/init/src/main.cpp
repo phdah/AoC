c36137e53c816ba7322367affd39aa4b2d787aad
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-12-04 19:40:46 +0100

    Have example input as default

--- 2024/init/src/main.cpp
+++ 2024/init/src/main.cpp
@@ -2,11 +2,13 @@
 #include <string>
 
 int main(int argc, char **argv) {
+    std::string fileName = "../example_input.txt";
+    if (argc > 1) fileName = argv[1];
 
-    int resultFirst = firstSolution(argv[1]);
+    int resultFirst = firstSolution(fileName);
     std::cout << "Result first: " << resultFirst << std::endl;
 
-    int resultSecond = secondSolution(argv[1]);
+    int resultSecond = secondSolution(fileName);
     std::cout << "Result second: " << resultSecond << std::endl;
 
     return 0;
