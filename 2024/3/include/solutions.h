73672bb82796f59b8e13064709bf94f71a0769f6
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-12-04 14:29:16 +0100

    UpdateSolution day three part two

--- 2024/3/include/solutions.h
+++ 2024/3/include/solutions.h
@@ -4,14 +4,38 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 int firstSolution(std::string fileName);
 int secondSolution(std::string fileName);
 
+enum MemoryState {
+    ENABLED,
+    DISABLED
+};
+
 class Memory {
     public:
-        static const int multiplyMatch(const std::smatch &match);
+        Memory(std::string &line): line{line} {}
+
+        const int calculateMatches(std::string::iterator beginIt, std::string::iterator endIt);
+
+        const int findNextInterval();
+
+    private:
+        std::string &line;
+        const std::string delDont = "don't()";
+        const std::string delDo = "do()";
+
+        std::string::iterator prevStrEnd = line.begin();
+        enum  MemoryState memoryState = ENABLED;
+
+        void switchMemoryState();
+        const std::string::iterator getDelEnd();
+        const std::string getSearchString();
+
+        static const int multiplyMatch(std::pair<std::string, std::string> &matchPair);
 
 };
 
@@ -24,9 +48,17 @@ class FileOps {
             }
         };
 
-        // Get the file
+        // Get the file contents as is
         std::ifstream &getFile() {
             return file;
+        };
+
+        // Get all lines from the file as one string
+        const std::string getFileContentAsOneString() {
+            std::string line(
+                (std::istreambuf_iterator<char>(this->file)),
+                std::istreambuf_iterator<char>());
+            return line;
         };
 
     private:
--- 2024/3/src/solutions.cpp
+++ 2024/3/src/solutions.cpp
@@ -1,4 +1,5 @@
 #include "solutions.h"
+#include <cstdio>
 #include <string>
 #include <regex>
 #include <utility>
@@ -6,29 +7,82 @@
  const int Memory::multiplyMatch(std::pair<std::string, std::string> &matchPair) {
     return std::stoi(matchPair.first) * std::stoi(matchPair.second);
 }
+void Memory::switchMemoryState() {
+    if (this->memoryState == MemoryState::ENABLED) {
+        this->memoryState = MemoryState::DISABLED;
+        return;
+    }
+
+    this->memoryState = MemoryState::ENABLED;
+    return;
 }
 
-int firstSolution(std::string fileName) {
-    FileOps file(fileName);
-    std::ifstream &fileContents = file.getFile();
-
+const int Memory::calculateMatches(std::string::iterator beginIt, std::string::iterator endIt) {
     // Match mul(123,456) and capture only the digits in groups
     const std::regex pat("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
     std::pair<std::string, std::string> matchPair;
 
+    // Create iterator search, using the iterators from the string
+    auto it = std::sregex_iterator(beginIt, endIt, pat);
+    auto end = std::sregex_iterator();
+
     int resultFirst = 0;
-    int mult;
-    std::string line;
-    while (std::getline(fileContents, line)) {
-        auto it = std::sregex_iterator(line.begin(), line.end(), pat);
-        auto end = std::sregex_iterator();
-        while (it != end) {
-            std::smatch match = *it;
-            matchPair = {match[1].str(), match[2].str()};
-            resultFirst += Memory::multiplyMatch(matchPair);
-            it++;
-        }
+    while (it != end) {
+        std::smatch match = *it;
+        matchPair = {match[1].str(), match[2].str()};
+        resultFirst += this->multiplyMatch(matchPair);
+        it++;
     }
+    return resultFirst;
+}
+
+const std::string Memory::getSearchString() {
+    if (this->memoryState == MemoryState::ENABLED) {
+        return this->delDont;
+    }
+    return this->delDo;
+};
+
+const std::string::iterator Memory::getDelEnd() {
+    std::string::iterator delEndRestricted;
+    std::size_t startPosNewSearch = this->prevStrEnd - this->line.begin();
+
+    std::size_t delEnd = this->line.find(this->getSearchString(), startPosNewSearch);
+    if (delEnd != std::string::npos) {
+        delEndRestricted = delEnd + this->line.begin();
+    } else {
+        delEndRestricted = this->line.end();
+    }
+    return delEndRestricted;
+};
+
+const int Memory::findNextInterval() {
+    int resultSecond = 0;
+    std::string::iterator delEnd;
+
+    while (this->prevStrEnd != line.end()) {
+        if (this->memoryState == MemoryState::ENABLED) {
+            delEnd = this->getDelEnd();
+            std::string newLine = line.substr(this->prevStrEnd-line.begin(), delEnd-line.begin());
+            resultSecond += this->calculateMatches(this->prevStrEnd, delEnd);
+        } else {
+            delEnd = this->getDelEnd();
+        }
+        // Update the previous starting pos and memory state
+        this->switchMemoryState();
+        this->prevStrEnd = delEnd;
+    }
+
+    return resultSecond;
+}
+
+int firstSolution(std::string fileName) {
+    FileOps file(fileName);
+    std::string line = file.getFileContentAsOneString();
+    Memory memory(line);
+
+    int resultFirst = 0;
+    resultFirst += memory.calculateMatches(line.begin(), line.end());
 
     return resultFirst;
 };
@@ -36,8 +90,11 @@ int firstSolution(std::string fileName) {
 
 int secondSolution(std::string fileName) {
     FileOps file(fileName);
-    std::ifstream &fileContents = file.getFile();
+    std::string line = file.getFileContentAsOneString();
+    Memory memory(line);
 
     int resultSecond = 0;
+    resultSecond += memory.findNextInterval();
+
     return resultSecond;
 }
